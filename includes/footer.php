dac9711aaae4d8a5248fd416a6ab71f0b7b6a53b
umar-developer <umamrd007@gmail.com>
2020-11-18 01:57:22 +0500

    [ini files added]

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+
+
+
+<!-- <footer class="jumbotron"> -->
+  
+  <!-- <h2> Hello world</h2> -->
+  
+  
+  <!-- </footer> -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  </body>
+  </html>
---
+++ includes/header.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon  -->
+    <link rel="shortcut icon" href="assests/img/favicons.jpg">
+    <!-- // Favicon  -->
+
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-5.14.0-web/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    <title>climate change</title>
+</head>
+
+
+
+<body>
+    <!-- navbar section -->
+     <!-- header section start -->
+     <nav class="navbar navbar-expand-lg navbar-transparent bg-transparent  custom-navbar" >
+         <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h5 class="font-weight-bold text-warning">Climate Change</h5></a>    
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto ">
+                <li class="nav-item  ">
+                    <a class="nav-link text-light" href="#">HOME</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link text-light " href="#">CLIMATE GLOBE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">DONATION</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light " href="#">CONTRIBUTORS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="#">CONTACT</a>
+                </li>
+
+
+            </ul>
+            <Form class="search-form ml-auto">
+                <span class="icon ion ion-search"></span>
+                <input class="form-control" type="text" placeholder="Search" >
+                
+            </Form>
+           
+        </div>
+         </div>
+    </nav>
+   
+    <!-- navbar section -->
+
+
+
+
+
+
